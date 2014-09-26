5f570b1908dd800d62cf325488d48835ce84bffc
Rohan Walker <rtrwalker@gmail.com>
2014-09-26 10:46:15 +1000

    set up constitutive_models package

---
+++ geotecha/constitutive_models/test/__init__.py
@@ -0,0 +1,23 @@
+# geotecha - A software suite for geotechncial engineering
+# Copyright (C) 2013  Rohan T. Walker (rtrwalker@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+"""
+This is the testing sub-package for the `constitutive_models` package.
+
+The sub-package contains the following modules:
+    - don't know yet
+    - don't know yet
+
+"""
