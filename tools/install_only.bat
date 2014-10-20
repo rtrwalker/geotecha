ae03d8037c079d0b98cfbb9285bf876adbdb4289
rtrwalker <rtrwalker@gmail.com>
2014-10-20 16:00:55 +1100

    Change some bat files to relative addresses

--- tools/install_only.bat
+++ tools/install_only.bat
@@ -1,12 +1,15 @@
 @REM "@" means don't display the commands that come after the "@".
 
 @ECHO ******************
-@ECHO Install geotecha python module
-@ECHO by Rohan Walker, October 2013
+@ECHO Build and Install geotecha python module (no testing)
+@ECHO by Rohan Walker, October 2014
 @ECHO ******************
-cd "C:\Users\Rohan Walker\Documents\GitHub\geotecha"
+@REM assuming run by clicking on bat file in tools directory of source
+cd ..
+python setup.py build --compiler=mingw32
 python setup.py install
 python setup.py clean --all
-@REM cd out of spec1d folder or nosetests will run on source code rather than installed pakcage (not exactly sure where nosetests searches first.  Drill down on current directory or what?)
+@pause
+
 
 
