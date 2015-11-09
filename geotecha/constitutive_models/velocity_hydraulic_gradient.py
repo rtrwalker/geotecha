a06f13d89a8ef722e388ef80fde30e977f14eb4d
rtrwalker <rtrwalker@gmail.com>
2015-11-09 11:23:15 +1100

    moved custom fixed point iteration to matematics.root_finding

--- geotecha/constitutive_models/velocity_hydraulic_gradient.py
+++ geotecha/constitutive_models/velocity_hydraulic_gradient.py
@@ -26,6 +26,8 @@ import matplotlib
 import scipy.optimize
 from collections import OrderedDict
 
+from geotecha.mathematics import root_finding
+
 class OneDimensionalFlowRelationship(object):
     """Base class for defining velocity vs hydraulic gradient relationships
     
@@ -483,9 +485,9 @@ class HansboNonDarcianFlowModel(OneDimensionalFlowRelationship):
                 # note as at 20151106 scipy.optimize.fixed_point may not 
                 # converge for this function because convergence acceleration 
                 # is used.  Use my modified version instead:
-                self.n = _fixed_point(fn,
-                                      1.00001,
-                                      args=(klinear, kstar, iL))                                                    
+                self.n = root_finding.fixed_point_no_accel(fn,
+                                                           1.00001,
+                                                           args=(klinear, kstar, iL))                                                    
 
 #                self.n = klinear / kstar / iL**(n - 1)
                 self.iL = iL
@@ -513,9 +515,9 @@ class HansboNonDarcianFlowModel(OneDimensionalFlowRelationship):
                 # note as at 20151106 scipy.optimize.fixed_point may not 
                 # converge for this function because convergence acceleration 
                 # is used.  Use my modified version instead:
-                self.n = _fixed_point(fn,
-                                      1.00001,
-                                      args=(klinear, kstar, i0))
+                self.n = root_finding.fixed_point_no_accel(fn,
+                                                           1.00001,
+                                                           args=(klinear, kstar, i0))
                                                     
                 self.iL = i0 * self.n / (self.n - 1)
                 self.i0 = i0
@@ -739,88 +741,6 @@ class HansboNonDarcianFlowModel(OneDimensionalFlowRelationship):
         x = np.linspace(xmin, xmax, npts)
         y = self.v_from_i(x)
         return x, y
-
-def fnn(_n, _klinear, _kstar, _i0):
-                    return _klinear / _kstar / (_i0 *_n/(_n - 1))**(_n-1)
-def fnn(_n, _klinear, _kstar, _i0):
-                    return np.log(_klinear/_kstar/_n) / np.log((_i0 *_n/(_n - 1))) + 1
-def fnn2(_n, _klinear, _kstar, _iL):
-                    return _klinear / _kstar / (_iL**(_n - 1))
-def fnn2(_n, _klinear, _kstar, _iL):
-                    return np.log(_klinear/_kstar/_n) / np.log(_iL) + 1
-                    
-def _fixed_point(func, x0, args=(), xtol=1e-8, maxiter=500):
-    """
-    Find a fixed point of the function (NO CONVEGENCE ACCELERATION!!!).
-    
-    Based on scipy.optimize.fixed_point for no convergence acelleration (RTRW)
-
-    Given a function of one or more variables and a starting point, find a
-    fixed-point of the function: i.e. where ``func(x0) == x0``.
-
-    Parameters
-    ----------
-    func : function
-        Function to evaluate.
-    x0 : array_like
-        Fixed point of function.
-    args : tuple, optional
-        Extra arguments to `func`.
-    xtol : float, optional
-        Convergence tolerance, defaults to 1e-08.
-    maxiter : int, optional
-        Maximum number of iterations, defaults to 500.
-
-    Notes
-    -----
-    Uses Steffensen's Method using Aitken's ``Del^2`` convergence acceleration.
-    See Burden, Faires, "Numerical Analysis", 5th edition, pg. 80
-
-    Examples
-    --------
-    >>> from scipy import optimize
-    >>> def func(x, c1, c2):
-    ...    return np.sqrt(c1/(x+c2))
-    >>>
-    >>> c1 = np.array([10,12.])
-    >>> c2 = np.array([3, 5.])
-    >>> optimize.fixed_point(func, [1.2, 1.3], args=(c1,c2))
-    array([ 1.4920333 ,  1.37228132])
-
-    """
-    if not isscalar(x0):
-        x0 = asarray(x0)
-        p0 = x0
-        for iter in range(maxiter):
-            p1 = func(p0, *args)            
-#            p2 = func(p1, *args)
-#            d = p2 - 2.0 * p1 + p0
-#            p = where(d == 0, p2, p0 - (p1 - p0)*(p1 - p0) / d)
-            p = p1
-            relerr = where(p0 == 0, p, (p-p0)/p0)
-            if all(abs(relerr) < xtol):
-                return p
-            p0 = p
-    else:
-        p0 = x0
-        for iter in range(maxiter):
-            p1 = func(p0, *args)
-#            p2 = func(p1, *args)
-#            d = p2 - 2.0 * p1 + p0
-#            if d == 0.0:
-#                return p2
-#            else:
-#                p = p0 - (p1 - p0)*(p1 - p0) / d
-            p=p1
-            if p0 == 0:
-                relerr = p
-            else:
-                relerr = (p - p0)/p0
-            if abs(relerr) < xtol:
-                return p
-            p0 = p
-    msg = "Failed to converge after %d iterations, value is %s" % (maxiter, p)
-    raise RuntimeError(msg)
 
                     
 if __name__ == "__main__":
